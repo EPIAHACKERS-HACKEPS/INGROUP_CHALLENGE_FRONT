7ec48ca99ed5d55d9c5c99ea13b52e306b086890
ismaDamm <issmaelhassouni@gmail.com>
2023-11-26 03:29:12 +0100

    estilos

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,5 +1,5 @@
 import React, { type ReactElement } from 'react'
-import { NavBarContainer, TitleDiv } from './styles'
+import { NavBarContainer, TitleDiv, TitleImg } from './styles'
 import { NavBarOption } from '../index'
 import { v4 as uuid } from 'uuid'
 
@@ -14,7 +14,7 @@ const NavBar = ({ options }: NavBarProps): ReactElement => {
   return (
     <NavBarContainer>
       <TitleDiv>
-        <h1>Admin</h1>
+        <TitleImg src='https://xrshop.store/cdn/shop/files/Logo-XRshop_a9bdf643-b213-499c-95b0-6edad8528cd9.png?v=1675854134&width=225'/>
       </TitleDiv>
       {
         options.map((option) => {
--- src/components/NavBarOption/styles.ts
+++ src/components/NavBarOption/styles.ts
@@ -5,7 +5,7 @@ interface NavBarOptionProps {
 }
 
 export const NavBarOptionText = styled.h3`
-  font-size: ${(props: NavBarOptionProps) => props.selected ? '2rem' : '1rem'};
+  font-size: ${(props: NavBarOptionProps) => props.selected ? '1.5rem' : '1.3rem'};
   margin: 0.5rem;
   padding: 0;
   font-family: 'Roboto', sans-serif;
