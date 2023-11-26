f9109384f4d64788ce7563533fa44211d73076c5
ismaDamm <issmaelhassouni@gmail.com>
2023-11-26 08:54:06 +0100

    ssisiis

--- src/components/DashboardSlide/Graphs/BarGraph/BarGraph.tsx
+++ src/components/DashboardSlide/Graphs/BarGraph/BarGraph.tsx
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, Cell } from 'recharts';
 
 // Define the type for the data items
@@ -7,42 +7,41 @@ interface IData {
   value: number;
 }
 
-// Data array with features and bugs
-const data: IData[] = [
-  { name: 'Feature', value: 6 },
-  { name: 'Bug', value: 4 },
-];
+const Example: React.FC = () => {
 
-const getColor = (name: string) => {
-  return name === 'Feature' ? 'green' : '#881d1c';
+    // Data array with features and bugs
+const data: IData[] = [
+    { name: 'Feature', value: 6 },
+    { name: 'Bug', value: 4 },
+  ];
+  
+  const getColor = (name: string) => {
+    return name === 'Feature' ? 'green' : '#881d1c';
+  };
+
+  
+  return (
+    <ResponsiveContainer width="100%" height="100%">
+      <BarChart
+        width={500}
+        height={300}
+        data={data}
+        margin={{
+          top: 5, right: 30, left: 20, bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Bar dataKey="value" fill="#8884d8">
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={getColor(entry.name)} />
+          ))}
+        </Bar>
+      </BarChart>
+    </ResponsiveContainer>
+  );
 };
 
-// Component class
-export default class Example extends PureComponent<{}, {}> {
-  render() {
-    return (
-      <ResponsiveContainer width="100%" height="100%">
-        <BarChart
-          width={500}
-          height={300}
-          data={data}
-          margin={{
-            top: 5, right: 30, left: 20, bottom: 5,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Bar dataKey="value" fill="#8884d8" >
-            {
-              data.map((entry, index) => (
-                <Cell key={`cell-${index}`} fill={getColor(entry.name)} />
-              ))
-            }
-          </Bar>
-        </BarChart>
-      </ResponsiveContainer>
-    );
-  }
-}
+export default Example;
