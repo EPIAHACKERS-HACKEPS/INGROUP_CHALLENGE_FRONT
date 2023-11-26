bab66531c34f184cfb5dffb2ee6ce9bed502fc06
ismaDamm <issmaelhassouni@gmail.com>
2023-11-26 05:36:46 +0100

    push

--- src/components/CommentsSlide/CommentsSlide.tsx
+++ src/components/CommentsSlide/CommentsSlide.tsx
@@ -1,12 +1,26 @@
 import React, { type ReactElement } from 'react'
 import UserStory from './UserStory/UserStory'
 import "./styles.css"
+import Items from './Items/Items'
+import Title from '../Title'
+import { Separator } from '../DashboardSlide/styles'
 
 const CommentsSlide = (): ReactElement => {
   return (
+    <>
+    <Title title={'Comments'} dark={true} />
+    <Separator />
     <div className='Container'>
-      <UserStory title={"Error graficos"} description={"Error en el motor grafico del gta6 creando un bug que elimina gran parte del progreso"}/>
+      <UserStory title={"Error graficos"} description={"Diferentes errores graficos"} />
+      <Items title="Error Graficos" description="Eliminas y no aparecen como eliminados" item="bug"/>
+      <Items title="Añadir esclavos" description="Porfa esclavos y negros, gracias!" item="feature"/>
     </div>
+    <div className='Container'>
+      <UserStory title={"Error graficos"} description={"Diferentes errores graficos"} />
+      <Items title="Error Graficos" description="Eliminas y no aparecen como eliminados" item="bug"/>
+      <Items title="Añadir esclavos" description="Porfa esclavos y negros, gracias!" item="feature"/>
+    </div>
+    </>
   )
 }
 
--- src/components/CommentsSlide/Items/Items.tsx
+++ src/components/CommentsSlide/Items/Items.tsx
@@ -3,15 +3,14 @@ import './styles.css';
 import { RxCross1 } from "react-icons/rx";
 
 
-export default function UserStory({ title, description }: any) {
+export default function Items({ title, description, item }: any) {
     return (
-        <div className="prueba">
+        <div className={`${item === 'bug' ? 'bug' : 'feature'}`}>
             <div className="titleDelete">
-                <p>User Story</p>
+                <p>{item}</p>
                 <div className="delete"><RxCross1 /></div>
             </div>
-            <div className="title">{title}
-            </div>
+            <div className="title">{title}</div>
             <div className="content">{description}</div>
         </div>
     );
